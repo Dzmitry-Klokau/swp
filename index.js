a3ed05f2a1c9ca9bb196ee4bda955a09620af417
Dzmitry Klokau <dzmitry.klokau@gmail.com>
2025-10-11 16:05:01 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+function sendToParentWindow(type, payload) {
+  window.parent.postMessage(
+    {
+      type,
+      payload,
+    },
+    "*"
+  );
+}
+function logToParent({ msg, level }) {
+  sendToParentWindow("log", {
+    msg: `[iframe] ${msg}`,
+    level,
+  });
+}
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./sw.js", {
+      scope: "./",
+    })
+    .then((reg) => {
+      logToParent({
+        msg: "SW is registered!",
+        level: "debug",
+      });
+    })
+    .catch((_err) => {
+      logToParent({
+        msg: `SW registration is not supported!`,
+        level: "error",
+      });
+    });
+} else {
+  logToParent({
+    msg: `SW is not registered. Error: ${err}.`,
+    level: "error",
+  });
+}
+
+window.addEventListener(
+  "message",
+  (event) => {
+    const data = event.data;
+
+    if (typeof data !== "object") {
+      return;
+    }
+    if (data.type !== "setSrc") {
+      return;
+    }
+    if (typeof data.url !== "string") {
+      return;
+    }
+    const frame = document.getElementById("swpFrame");
+    const src = `https://dzmitry-klokau.github.io/swp?url=${
+      data.url
+    }&randomValue=${Math.floor(Math.random() * 1001)}`;
+    frame.src = src;
+    frame.style.display = "block";
+
+    logToParent({
+      msg: `New iframe url is ${src}`,
+      level: "debug",
+    });
+  },
+  false
+);
+
+navigator.serviceWorker.addEventListener("message", async (event) => {
+  const port = event.ports[0];
+  const data = event.data;
+
+  if (data.type === "swp-request") {
+    const url = data.url;
+
+    sendToParentWindow("swp-request", url);
+
+    window.addEventListener("message", function handler(e) {
+      if (e.data.type === "swp-response" && e.data.url === url) {
+        port.postMessage(e.data.response);
+        window.removeEventListener("message", handler);
+      }
+    });
+  }
+  if (data.type === "log") {
+    sendToParentWindow("log", data.payload);
+  }
+});
---
+++ sw.js
@@ -0,0 +1,63 @@
+function postMessageToAllClients(msgObj, options) {
+  self.clients
+    .matchAll({ type: "window", includeUncontrolled: true })
+    .then((clients) => {
+      for (const client of clients) {
+        client.postMessage(msgObj, options);
+      }
+    });
+}
+
+function logMessage(msg, level) {
+  postMessageToAllClients({
+    type: "log",
+    payload: {
+      msg: `[sw] ${msg}`,
+      level,
+    },
+  });
+}
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(self.clients.claim());
+  logMessage("SW activated", "debug");
+});
+
+self.addEventListener("fetch", (event) => {
+  const reqUrl = new URL(event.request.url);
+  const url = reqUrl.searchParams.get("url");
+  if (url) {
+    event.respondWith(handleProxyRequest(url));
+  }
+});
+
+async function handleProxyRequest(url) {
+  try {
+    logMessage(`Process ${url}`, "debug");
+    const messageChannel = new MessageChannel();
+
+    const responsePromise = new Promise((resolve) => {
+      messageChannel.port1.onmessage = (event) => {
+        const { body, status, headers } = event.data;
+        resolve(new Response(body, { status, headers }));
+      };
+    });
+
+    postMessageToAllClients(
+      {
+        type: "swp-request",
+        url,
+      },
+      [messageChannel.port2]
+    );
+
+    return await responsePromise;
+  } catch (err) {
+    logMessage(`Error: ${err?.message}`, "error");
+    return new Response("Internal error", { status: 500 });
+  }
+}
