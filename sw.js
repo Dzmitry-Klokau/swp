82d683907b5c5b485db82bea1465e0d97eaaed94
Dzmitry-Klokau <71276547+Dzmitry-Klokau@users.noreply.github.com>
2025-10-23 18:30:47 +0300

    add log

--- sw.js
+++ sw.js
@@ -5,6 +5,9 @@ function postMessageToAllClients(msgObj, options) {
       for (const client of clients) {
         client.postMessage(msgObj, options);
       }
+      if (clients.length === 0) {
+        alert("no clients");
+      }
     });
 }
 
